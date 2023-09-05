dfab1f7d3a280e402169f64b4b72a0dec6a410c2
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-05 18:28:50 +0200

    adding keyframes for individuals

--- frontend-images/src/components/Individuals/IndividualsResults.js
+++ frontend-images/src/components/Individuals/IndividualsResults.js
@@ -319,7 +319,9 @@ function IndividualsResults(props) {
 
     }
     return (
-        <div>{timeOut === false && <div className="contenidorvolta">
+
+        <div>
+                    {timeOut === false && <div className="contenidorvolta">
         <div className="volta"></div>
       </div>}
             {logInRequired === false &&
